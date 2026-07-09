c575e7aa2a0b5ede38c23389854da218a41999e9
Aadil Qureshi <36612205+aadilq@users.noreply.github.com>
2026-07-09 12:38:59 -0400

    variables.tf stated

---
+++ terraform/variables.tf
@@ -0,0 +1,20 @@
+variable "project_id" {
+    description = "GCP project ID"
+    default = "video-metadata-pipeline"
+}
+
+variable "region" {
+    description = "GCP region"
+    default = "us-central1
+}
+
+variable "db_password" {
+    description = "Cloud SQL postgres password"
+    sensitive   = true
+}
+
+variable "webhook_url" {
+    description = "Webhook URL for downstream notifications"
+    sensitive   = true
+}
+
